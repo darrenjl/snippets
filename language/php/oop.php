69450a9da98c1ebf8f57a933ad92a8078afdabcc
Darren Lyons <d.lyons88@gmail.com>
2020-08-08 21:31:47 +1000

    Adding file to store OOP snippets

---
+++ language/php/oop.php
@@ -0,0 +1,45 @@
+<?php
+
+class SimpleClass
+{
+    // property declaration
+    public $var = 'a default value';
+
+    // method declaration
+    public function displayVar() {
+        echo $this->var . PHP_EOL;
+    }
+}
+
+$simp = new SimpleClass();
+$simp->displayVar();
+
+class Foo
+{
+    public function printItem($string)
+    {
+        echo 'Foo: ' . $string . PHP_EOL;
+    }
+    
+    public function printPHP()
+    {
+        echo 'PHP is great.' . PHP_EOL;
+    }
+}
+
+class Bar extends Foo
+{
+    public function printItem($string)
+    {
+        echo 'Bar: ' . $string . PHP_EOL;
+    }
+}
+
+$foo = new Foo();
+$bar = new Bar();
+$foo->printItem('baz'); // Output: 'Foo: baz'
+$foo->printPHP();       // Output: 'PHP is great' 
+$bar->printItem('baz'); // Output: 'Bar: baz'
+$bar->printPHP();       // Output: 'PHP is great'
+
+?>
